fa25ab5c5a0296fb2e5ceddec3f3be919b26fb66
Willy Lee <williamlee080508@gmail.com>
2025-05-25 00:36:19 -0400

    github

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
       <body className="tracking-tighter">
         {children}
         <div className="mt-20 ml-20 mr-20 border-t-[1px] mb-20 border-t-gray-300 block pt-2 text-sm text-gray-400">
-          <a href="">See the Github repository</a>
+          <a href="https://github.com/willyl3e/physics">See the Github repository</a>
         </div>
       </body>
     </html>
